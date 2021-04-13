f7ad078851954b1892d4fc60ad71644a5548a141
daink671 <dainkim.974@gmail.com>
2021-04-13 17:25:33 +0000

    until delete

--- public/javascripts/delete.js
+++ public/javascripts/delete.js
@@ -1,3 +1,35 @@
-function processResult() {
-  window.alert("Product deleted!");
+document.getElementById("delete").onclick = function () {
+    const product = document.getElementById("product-id").value;
+    axios
+        .delete(`/api/products/${product}`)
+        .then(processResult)
+        .catch((err) => {
+            if (err.response.status === 404) {
+                notFound();
+            }
+        });
 }
+
+function processResult() {
+    window.alert("Product deleted!");
+}
+
+function notFound() {
+  resetContentArea();
+
+  const h2 = document.querySelector("h2.hidden");
+  h2.className = "";
+}
+
+function resetContentArea() {
+  document.querySelector("h2").className = "hidden";
+  const currentProduct = document.getElementById("product-result");
+  if (currentProduct) {
+    currentProduct.remove();
+  }
+  const productList = document.getElementById("product-list");
+  if (productList) {
+    productList.remove();
+  }
+}
+
--- routes/client.js
+++ routes/client.js
@@ -4,12 +4,9 @@ var router = express.Router();
 router.get("/", function (req, res) {
   res.sendFile("read.html", { root: "views" });
 });
+
 router.get("/read", function (req, res) {
   res.sendFile("read.html", { root: "views" });
-});
-
-router.get("/search", function (req, res, next) {
-  res.sendFile("search.html", { root: "views" });
 });
 
 router.get("/create", function (req, res) {
